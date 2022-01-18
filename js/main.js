774d9ec705c1f9c610e1b6aa8a369d8d66efd96e
Jakub Zykl <jakub.zykl@gmail.com>
2022-01-18 19:42:06 +0100

    commit #2

---
+++ js/main.js
@@ -0,0 +1,28 @@
+(function($) {
+
+    "use strict";
+
+    $('.language').hover(function() {
+        var $this = $(this);
+        $this.addClass('show');
+        $this.find('> a').attr('aria-expanded', true);
+        $this.find('.dropdown-menu').addClass('show');
+    }, function() {
+        var $this = $(this);
+        $this.removeClass('show');
+        $this.find('> a').attr('aria-expanded', false);
+        $this.find('.dropdown-menu').removeClass('show');
+    });
+    $('nav .dropdown').hover(function() {
+        var $this = $(this);
+        $this.addClass('show');
+        $this.find('> a').attr('aria-expanded', true);
+        $this.find('.dropdown-menu').addClass('show');
+    }, function() {
+        var $this = $(this);
+        $this.removeClass('show');
+        $this.find('> a').attr('aria-expanded', false);
+        $this.find('.dropdown-menu').removeClass('show');
+    });
+
+})(jQuery);
--- readme.md
+++ readme.md
@@ -3,3 +3,10 @@ Building Pavel Hrňa's web
 
 skript pro kompilaci scss: 
 sass --watch scss/main.scss:css/style.css 
+
+## TODO:
+    - meta tagy
+    - google analytics
+    -
+
+## Změny
