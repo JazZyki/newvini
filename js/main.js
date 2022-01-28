0ce64d8a1ad8921d54befeb865d1140208c13ede
Jakub Zykl <jakub.zykl@gmail.com>
2022-01-28 15:29:29 +0100

    commit #9 - contact page finished

--- js/main.js
+++ js/main.js
@@ -58,3 +58,9 @@ $(function() {
         $('.' + officeKey).show();
     });
 });
+
+// Initialize tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
